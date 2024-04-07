4e4fa07e363789f010f8a50cee65ab37a3f5edf6
jongwon-kr <growjong8802@gmail.com>
2024-04-07 16:36:16 +0900

    start restapi service coin

---
+++ src/main/java/com/jongwon/coinProject/CoinInfoServiceApplication.java
@@ -0,0 +1,13 @@
+package com.jongwon.coinProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoinInfoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoinInfoServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jongwon/coinProject/CoinInfoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jongwon.coinProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoinInfoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
